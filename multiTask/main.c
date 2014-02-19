f0223b92052c14b7d222bfb0d3bef90ff3598c55
Gustave Monod <gustave.monod@insa-lyon.fr>
2014-02-19 14:02:52 +0100

    Initial commit.

---
+++ multiTask/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  multiTask
+//
+//  Created by Gustave Monod on 18/02/14.
+//  Copyright (c) 2014 Yannick Marion & Gustave Monod. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main( )
+{
+	printf( "Hello, world!" );
+	return 0;
+}
