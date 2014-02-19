70c05c6b7561b44e7e01dd7445844378bde0c756
Gustave Monod <gustave.monod@insa-lyon.fr>
2014-02-19 15:12:51 +0100

    Added the skeleton.

---
+++ multiTask/Mother.c
@@ -0,0 +1,49 @@
+/*************************************************************************
+   Mother  -  The mother task creating the context and launching the app
+                             -------------------
+    date                 : Feb. 19 2014
+    copyright            : (C) 2014 Yannick Marion & Gustave Monod
+    e-mail               : yannick.marion@insa-lyon.fr
+                           gustave.monod@insa-lyon.fr
+*************************************************************************/
+
+//---------- Realization of the <Mother> task (file Mother.cpp) ----------
+
+/////////////////////////////////////////////////////////////////  INCLUDE
+//--------------------------------------------------------- System include
+#include <stdio.h>
+
+//------------------------------------------------------- Personal include
+#include "Mother.h"
+
+/////////////////////////////////////////////////////////////////  PRIVATE
+//-------------------------------------------------------------- Constants
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------- Static variables
+
+//------------------------------------------------------ Private functions
+//static type name ( parameter list )
+// How to use:
+//
+// Contract:
+//
+// Algorithm:
+//
+//{
+//} //----- End of name
+
+//////////////////////////////////////////////////////////////////  PUBLIC
+//------------------------------------------------------- Public functions
+// type Name ( parameter list )
+// Algorithm:
+//
+//{
+//} //----- End of Name
+
+int main ( )
+{
+	printf( "Hello, world!" );
+	return 0;
+}
---
+++ multiTask/Mother.h
@@ -0,0 +1,42 @@
+/*************************************************************************
+   Mother  -  The mother task creating the context and launching the app
+                             -------------------
+    date                 : Feb. 19 2014
+    copyright            : (C) 2014 Yannick Marion & Gustave Monod
+    e-mail               : yannick.marion@insa-lyon.fr
+                           gustave.monod@insa-lyon.fr
+*************************************************************************/
+
+//---------- Interface of the <Mother> task (file Mother.h) --------------
+#if !defined ( MOTHER_H )
+#define MOTHER_H
+
+//------------------------------------------------------------------------
+// Role of the <Mother> task
+//
+// Creates and destroys:
+// * Keyboard
+//------------------------------------------------------------------------
+
+/////////////////////////////////////////////////////////////////  INCLUDE
+//-------------------------------------------------------- Used interfaces
+
+//-------------------------------------------------------------- Constants
+
+//------------------------------------------------------------------ Types
+
+//////////////////////////////////////////////////////////////////  PUBLIC
+//------------------------------------------------------- Public functions
+int main ( );
+// How to use:
+//
+// Contract:
+
+// type Name ( parameter list );
+// How to use:
+//
+// Contract:
+//
+
+#endif // MOTHER_H
+
